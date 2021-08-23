4628d53dcf3854af529477c95c1a52a689d26c07
ashish-jaiswar <88731979+ashish-jaiswar@users.noreply.github.com>
2021-08-23 14:41:34 +0530

    Create Rotate.java

---
+++ Rotate.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Rotate {
+//  search number in rotated sorted array
+    static int Pivot(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] ==target) {
+                return mid;
+            }
+            if (arr[start] < arr[mid]) {
+                if(target>=arr[start]&& target<arr[mid]){
+                    end=mid-1;
+                }
+                else {
+                    start=mid+1;
+                }
+            } else {
+                if (target > arr[mid] && target < arr[end]) {
+                    start = mid - 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+       return -1;
+        }
+    public static void main(String[] args) {
+        // write your code here
+        int arr[] = {4,5,6,7,0,1,2,3};
+        int n=3;
+      int result= Main.Pivot(arr,n);
+        System.out.println(result);
+    }
+   }
+
